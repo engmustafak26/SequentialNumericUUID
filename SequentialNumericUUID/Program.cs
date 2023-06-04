f169cf7b7b1e4d95862001510f569ae72e6d8327
mustafa kamal <eng.mustafak26>
2023-06-04 12:04:23 +0300

    Add project files.

---
+++ SequentialNumericUUID/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SequentialNumericUUID;
+
+
+Console.WriteLine("  Generate 100 Sequential Numbers:-");
+for (int i = 0; i < 100; i++)
+{
+    ulong nanoSecondsTimeStamp = SequentialNumericUID.Generate();
+    Console.WriteLine("Nano Timestamp: "+nanoSecondsTimeStamp);
+    Console.WriteLine("Date UTC :"+SequentialNumericUID.GetUTCDateTime(nanoSecondsTimeStamp).ToString());
+    Console.WriteLine();
+}
+
+
---
+++ SequentialNumericUUID/SequentialNumericUID.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialNumericUUID
+{
+    public static class SequentialNumericUID
+    {
+        const short CustomUniversalStartYear = 2023;
+        const byte MicroPlusNanoDigitsLength = 4;
+        private static DateTime CustomUniversalTime = new DateTime(CustomUniversalStartYear, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static ulong Generate()
+        {
+            DateTime current = DateTime.UtcNow;
+            long timeStamp = (long)current.Subtract(CustomUniversalTime).TotalMilliseconds;
+            return ulong.Parse($"{timeStamp.ToString()}{current.Microsecond.ToString("D3")}{(current.Nanosecond / 100).ToString()}");
+        }
+
+        public static DateTime GetUTCDateTime(ulong sequentialNumericUUID)
+        {
+            string customTimeStampString = sequentialNumericUUID.ToString();
+            customTimeStampString = customTimeStampString.Substring(0, customTimeStampString.Length - MicroPlusNanoDigitsLength);
+            return CustomUniversalTime.AddMilliseconds(ulong.Parse(customTimeStampString));
+        }
+
+        public static DateTime GetLocalDateTime(ulong sequentialNumericUUID)
+        {
+            return GetUTCDateTime(sequentialNumericUUID).ToLocalTime();
+        }
+    }
+}
