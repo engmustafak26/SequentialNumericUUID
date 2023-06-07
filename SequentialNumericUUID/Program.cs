111419ae7cfee8fb15f76e0e1cf43634c3273061
mustafa kamal <eng.mustafak26>
2023-06-07 15:18:28 +0300

    add  random NanoSecond

--- SequentialNumericUUID/Program.cs
+++ SequentialNumericUUID/Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SequentialNumericUUID;
 
-
 Console.WriteLine("  Generate 100 Sequential Numbers:-");
 for (int i = 0; i < 100; i++)
 {
     ulong nanoSecondsTimeStamp = SequentialNumericUID.Generate();
-    Console.WriteLine("Nano Timestamp: "+nanoSecondsTimeStamp);
-    Console.WriteLine("Date UTC :"+SequentialNumericUID.GetUTCDateTime(nanoSecondsTimeStamp).ToString());
+    ulong randomNanoSecondsTimeStamp = SequentialNumericUID.GenerateWithRandomnessInNanoSecondScope();
+    Console.WriteLine("Nano Timestamp:       " + nanoSecondsTimeStamp);
+    Console.WriteLine("Random Nano Timestamp:" + randomNanoSecondsTimeStamp);
+    Console.WriteLine("Date UTC :" + SequentialNumericUID.GetUTCDateTime(nanoSecondsTimeStamp).ToString());
+    Console.WriteLine("Date UTC :" + SequentialNumericUID.GetUTCDateTime(randomNanoSecondsTimeStamp, true).ToString());
     Console.WriteLine();
 }
 
--- SequentialNumericUUID/SequentialNumericUID.cs
+++ SequentialNumericUUID/SequentialNumericUID.cs
@@ -10,6 +10,8 @@ namespace SequentialNumericUUID
     {
         const short CustomUniversalStartYear = 2023;
         const byte MicroPlusNanoDigitsLength = 4;
+        const byte RandomnessDigitsLength = 4;
+        const byte ULongDigitsCount = 20;
         private static DateTime CustomUniversalTime = new DateTime(CustomUniversalStartYear, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static ulong Generate()
@@ -19,10 +21,24 @@ namespace SequentialNumericUUID
             return ulong.Parse($"{timeStamp.ToString()}{current.Microsecond.ToString("D3")}{(current.Nanosecond / 100).ToString()}");
         }
 
-        public static DateTime GetUTCDateTime(ulong sequentialNumericUUID)
+        public static ulong GenerateWithRandomnessInNanoSecondScope()
+        {
+            DateTime current = DateTime.UtcNow;
+            long timeStamp = (long)current.Subtract(CustomUniversalTime).TotalMilliseconds;
+
+            string ulongSegment = $"{timeStamp.ToString()}{current.Microsecond.ToString("D3")}{(current.Nanosecond / 100).ToString()}";
+            string randomString = Random.Shared.Next(0, 9999).ToString();
+            int recomendedRandomStringSliceLength = ULongDigitsCount - ulongSegment.Length;
+            recomendedRandomStringSliceLength = recomendedRandomStringSliceLength > randomString.Length ? randomString.Length : recomendedRandomStringSliceLength;
+
+            return ulong.Parse($"{ulongSegment}{randomString.Substring(0, recomendedRandomStringSliceLength).PadLeft(5 - recomendedRandomStringSliceLength, '0')}");
+        }
+
+        public static DateTime GetUTCDateTime(ulong sequentialNumericUUID, bool isGeneratedWithRandomness = false)
         {
             string customTimeStampString = sequentialNumericUUID.ToString();
-            customTimeStampString = customTimeStampString.Substring(0, customTimeStampString.Length - MicroPlusNanoDigitsLength);
+            customTimeStampString = customTimeStampString.Substring(0, customTimeStampString.Length 
+                                                            - (isGeneratedWithRandomness ? (RandomnessDigitsLength + MicroPlusNanoDigitsLength) : MicroPlusNanoDigitsLength));
             return CustomUniversalTime.AddMilliseconds(ulong.Parse(customTimeStampString));
         }
 
