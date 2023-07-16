4480c8c29504a17b6267c36af721f186fb727009
mustafa kamal <eng.mustafak26>
2023-07-16 13:33:13 +0300

    narrow generated uuid

--- SequentialNumericUUID/SequentialNumericUID.cs
+++ SequentialNumericUUID/SequentialNumericUID.cs
@@ -12,13 +12,15 @@ namespace SequentialNumericUUID
         const byte MicroPlusNanoDigitsLength = 4;
         const byte RandomnessDigitsLength = 4;
         const byte ULongDigitsCount = 20;
+        const ulong Seed = 169720846503714;
+        const ulong RandomNanoSeed = 1697225263413546208;
         private static DateTime CustomUniversalTime = new DateTime(CustomUniversalStartYear, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static ulong Generate()
         {
             DateTime current = DateTime.UtcNow;
             long timeStamp = (long)current.Subtract(CustomUniversalTime).TotalMilliseconds;
-            return ulong.Parse($"{timeStamp.ToString()}{current.Microsecond.ToString("D3")}{(current.Nanosecond / 100).ToString()}");
+            return ulong.Parse($"{timeStamp.ToString()}{current.Microsecond.ToString("D3")}{(current.Nanosecond / 100).ToString()}") - Seed;
         }
 
         public static ulong GenerateWithRandomnessInNanoSecondScope()
@@ -29,17 +31,18 @@ namespace SequentialNumericUUID
             string ulongSegment = $"{timeStamp.ToString()}{current.Microsecond.ToString("D3")}{(current.Nanosecond / 100).ToString()}";
             string randomString = Random.Shared.Next(0, 9999).ToString();
             int recomendedRandomStringSliceLength = ULongDigitsCount - ulongSegment.Length;
-            recomendedRandomStringSliceLength = recomendedRandomStringSliceLength > randomString.Length ? 
+            recomendedRandomStringSliceLength = recomendedRandomStringSliceLength > randomString.Length ?
                                                                                     randomString.Length : recomendedRandomStringSliceLength;
 
             return ulong.Parse($"{ulongSegment}{randomString.Substring(0, recomendedRandomStringSliceLength)
-                                                            .PadLeft(5 - recomendedRandomStringSliceLength, '0')}");
+                                                            .PadLeft(5 - recomendedRandomStringSliceLength, '0')}") - RandomNanoSeed;
         }
 
         public static DateTime GetUTCDateTime(ulong sequentialNumericUUID, bool isGeneratedWithRandomness = false)
         {
+            sequentialNumericUUID += isGeneratedWithRandomness ? RandomNanoSeed : Seed;
             string customTimeStampString = sequentialNumericUUID.ToString();
-            customTimeStampString = customTimeStampString.Substring(0, customTimeStampString.Length 
+            customTimeStampString = customTimeStampString.Substring(0, customTimeStampString.Length
                                                             - (isGeneratedWithRandomness ? (RandomnessDigitsLength + MicroPlusNanoDigitsLength) : MicroPlusNanoDigitsLength));
             return CustomUniversalTime.AddMilliseconds(ulong.Parse(customTimeStampString));
         }
